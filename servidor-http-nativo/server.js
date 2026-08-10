a35e546d5b64c313527a45a8b89f1c8a565d6257
mariamella-11 <maria.mella@estudante.ifms.edu.br>
2026-08-10 21:19:25 +0000

    Servidor criado

---
+++ servidor-http-nativo/server.js
@@ -0,0 +1,14 @@
+const http = require('node:http');
+const PORTA = 3000;
+const server = http.createServer((req, res) => {
+    console.log(`Requisição recebida! ${req.method} ${req.url}`)
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+
+    res.end("Servidor nativo funcionando!");
+});
+
+server.listen(PORTA, () => {
+    console.log(`Servidor funcionando na porta ${PORTA}`)
+})
